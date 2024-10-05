444c81dcddc76d3711e0a39315d213ba5d0e1211
Name Surname <your@email>
2024-10-05 21:52:14 +0300

    Добавил документацию

--- circle.py
+++ circle.py
@@ -1,10 +1,20 @@
 import math
 
-
 def area(r):
+    """
+    Вычисляет площадь круга.
+
+    :param r: Радиус круга.
+    :return: Площадь круга.
+    """
     return math.pi * r * r
 
 
 def perimeter(r):
-    return 2 * math.pi * r
+    """
+    Вычисляет периметр (длину окружности) круга.
 
+    :param r: Радиус круга.
+    :return: Длина окружности.
+    """
+    return 2 * math.pi * r
